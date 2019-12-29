5ae509fdddd9867d516f73a92e3feb83685fb92a
adi791 <adrian.s516401828@gmail.com>
2019-12-29 01:46:54 +0100

    thunderbird has been upgrade

--- install-thunderbird.sh
+++ install-thunderbird.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 sudo pacman -S thunderbird
-
+sudo pacman -S thunderbird-i18n-pl
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
